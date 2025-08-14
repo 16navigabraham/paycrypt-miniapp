8fd8a325bb0602a363fa59d0da715a622b457933
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-08-14 13:23:03 +0100

    hello

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -38,7 +38,7 @@ export async function GET() {
       ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE_URL,
-      castShareUrl: process.env.NEXT_PUBLIC_APP_CAST_SHARE_URL,
+      castShareUrl: URL,
       screenshotUrl: process.env.NEXT_PUBLIC_APP_SCREENSHOT_URLS,
       buttonTitle: process.env.NEXT_PUBLIC_APP_BUTTON_TITLE,
       imageUrl: process.env.NEXT_PUBLIC_APP_IMAGE_URL,
